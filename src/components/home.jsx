0305d6fd830777c52e770031eef1090cb80b7349
MSK4477 <ks7997067@gmail.com>
2023-10-21 17:13:31 +0530

    completed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,7 @@
 import homeSvg from "../svg/designer-9c.svg";
 import TypeWriterEffect from 'typewriter-effect';
-
-
+import resume from "../svg/resume.Saravana.pdf"
+ 
 const Home = () => {
 
   return (
@@ -26,6 +26,7 @@ const Home = () => {
           />
           </h1>
           <img src={homeSvg} alt="homeImg" />
+          <a id="resume" href={resume} download="resume">Resume</a>
     </div>
   );
 };
